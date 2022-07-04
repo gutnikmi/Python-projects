230f2a5986856d117bc5598d97d81f86663f454c
gutnikmi <91624431+gutnikmi@users.noreply.github.com>
2022-07-04 20:25:06 +0300

    Update rps_tki.py

--- rps/rps_tki.py
+++ rps/rps_tki.py
@@ -1,4 +1,4 @@
-from rps import rps_intg
+from rps_int_re import rps_intg
 from tkinter import *
 from PIL import ImageTk, Image
 import warnings
@@ -44,7 +44,10 @@ class ChooseFig:
 
 class CompChoice:
     def comp_turn(self):
-        self.a = chs.a - 1
+        if chs.a != 1:
+            self.a = chs.a - 1
+        else:
+            self.a = 3
         self.res, self.c_c = rps_intg(self.a)
         match self.res:
             case "w":
@@ -88,63 +91,3 @@ cchs.comp_turn()
 label = Label(text=" ", padx="70", pady="10", font="1")
 label.grid(row=0, column=0, padx=8, pady=0)
 root.mainloop()
-
-# def choose_pic():
-#     global a
-#     global c
-#     global img, img2
-#     global res
-#     global c_c
-#     match a:
-#         case 1:
-#             img = Image.open('Rock.png')
-#             img = img.resize((150, 150), Image.LANCZOS)
-#             img = ImageTk.PhotoImage(img)
-#             c = "r"
-#         case 2:
-#             img = Image.open('Paper.png')
-#             img = img.resize((150, 150), Image.LANCZOS)
-#             img = ImageTk.PhotoImage(img)
-#             c = "p"
-#         case 3:
-#             img = Image.open('Scissors.png')
-#             img = img.resize((150, 150), Image.LANCZOS)
-#             img = ImageTk.PhotoImage(img)
-#             c = "s"
-#         case _:
-#             print('You fucked up')
-#     if a < 3:
-#         a += 1
-#     else:
-#         a = 1
-#     res, c_c = rps_intg(c)
-#     match res:
-#         case "w":
-#             res = "You win!"
-#         case "d":
-#             res = "It's a draw!"
-#         case "l":
-#             res = "You loose"
-#
-#     btn = Button(image=img, padx="5", pady="0", font="16", command=choose_pic)
-#     btn.grid(row=1, column=1, padx=8, pady=0)
-#     match c_c:
-#         case "r":
-#             img2 = Image.open('Rock.png')
-#             img2 = img2.resize((150, 150), Image.LANCZOS)
-#             img2 = ImageTk.PhotoImage(img2)
-#         case "p":
-#             img2 = Image.open('Paper.png')
-#             img2 = img2.resize((150, 150), Image.LANCZOS)
-#             img2 = ImageTk.PhotoImage(img2)
-#         case "s":
-#             img2 = Image.open('Scissors.png')
-#             img2 = img2.resize((150, 150), Image.LANCZOS)
-#             img2 = ImageTk.PhotoImage(img2)
-#         case _:
-#             print('You fucked up')
-#
-#     lbl = Label(text=res, padx="80", pady="10", font="1")
-#     lbl.grid(row=2, column=1, padx=8, pady=0)
-#     lbl = Label(image=img2, padx="80", pady="10", font="1")
-#     lbl.grid(row=3, column=1, padx=8, pady=0)
