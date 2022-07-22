4c8127afe64b72a827af3b84115f206229df354f
Mitia <gutnikmi@mail.ru>
2022-07-22 19:58:34 +0300

    added aes prototype

--- Cryptoserv opt/AES test.py
+++ Cryptoserv opt/AES test.py
@@ -1,17 +1,19 @@
+import os
 from Crypto.Cipher import AES
 
-
+key = os.urandom(16) # l = 128
+print(key)
 BS = 16
 pad = lambda s: s + (BS - len(s) % BS) * chr(BS - len(s) % BS)
 unpad = lambda s : s[:-ord(s[len(s)-1:])]
-key = 'abcdefghijklmnop'
-cipher = AES.new(key.encode(), AES.MODE_ECB)
+#key = 'abcdefghijklmnop'
+cipher = AES.new(key, AES.MODE_ECB)
 msg = 'test'
 msg = pad(msg)
 msg = cipher.encrypt(msg.encode())
 print(msg)
 
-decipher = AES.new(key.encode(), AES.MODE_ECB)
+decipher = AES.new(key, AES.MODE_ECB)
 print(unpad(decipher.decrypt(msg).decode()))
 
 
