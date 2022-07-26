5b3bdfad322dfbb74f7b0cfcdd72f38a967e45ab
Mitia <gutnikmi@mail.ru>
2022-07-26 13:16:50 +0300

    added -h command

--- Cryptoserv opt/Server/server.py
+++ Cryptoserv opt/Server/server.py
@@ -81,6 +81,10 @@ def serv_cmd(inpt):  # parse commands
             files_serv = os.listdir()
             files_serv = '\n'.join(files_serv)
             return files_serv
+        case "-h":
+            cmd_list = "-l: lists all files stored on the server \n" \
+                       "-h: lists all server commands"
+            return cmd_list
         case _:
             return "Invalid command, write -h for list of commands"
 
