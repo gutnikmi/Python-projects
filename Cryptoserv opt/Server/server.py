766cc5e74566a02e1ac158795cf4960fc471e705
Mitia <gutnikmi@mail.ru>
2022-07-27 14:18:04 +0300

    server commands are now in a separate file

--- Cryptoserv opt/Server/server.py
+++ Cryptoserv opt/Server/server.py
@@ -1,6 +1,7 @@
 import socket, time, rsa, pickle, os
 from Crypto.Cipher import AES
 from Crypto.Util.Padding import pad, unpad
+from serv_commands import serv_cmd
 
 
 def enc_pic(data):  # encrypt and pickle
@@ -60,7 +61,7 @@ def con_handle():  # handles received data, creates new connections
         return data
     except Exception as e:
         print(e)
-        print("Bad data, listening for new connections")
+        print("Connection closed, listening for new connections")
         con.sock.close()
         new_con()
         send_ks(pub)
@@ -70,59 +71,6 @@ def con_handle():  # handles received data, creates new connections
         data = b'Errors detected '
         data = rsa.encrypt(data, pub)
         return data
-
-
-def serv_cmd(inpt):  # parse commands
-    func_dict = {
-        "-l": list_f,
-        "-h": help_f,
-    }
-
-    if " " in inpt:
-        inpt, args = inpt.split(" ", 1)
-        if inpt in func_dict:
-            return func_dict[inpt](args)
-        else:
-            return "Unknown command, type -h to list all available commands"
-    else:
-        if inpt in func_dict:
-            return func_dict[inpt]()
-        else:
-            return "Unknown command, type -h to list all available commands"
-
-
-def arg_filter(args, allowed):
-    res = ""
-    for i in args:
-        if (i != " " and i != "-") and (f"-{i}" not in allowed):
-            res += f"-{i} is not an argument, type -h -l for all valid args \n"
-    return res
-
-
-def list_f(args=''):
-    res = ""
-    allowed = ["-h", "-l"]
-    res += arg_filter(args, allowed)
-    if "-h" in args:
-        res += "this function lists all files on the server.\n"
-    if args == '' or "-l" in args:
-        files_serv = '\n'.join(os.listdir()) + "\n"
-        res += files_serv
-    return res
-
-
-def help_f(args=''):
-    res = ""
-    allowed = ["-h", "-l"]
-    res += arg_filter(args, allowed)
-    if "-l" in args:  # optional args
-        res += "-l: lists all files stored on the server.\n" \
-               "accepts arguments:\n" \
-               "-h: description \n"
-    if args == '' or "-h" in args:  # default arg
-        res += "-l: lists all files stored on the server \n" \
-                   "-h: lists all server commands"
-    return res
 
 
 class Cnct:  # settings for a connection
