4926da822a8dea255cc201cbb2c7f0fc8204e946
Mitia <gutnikmi@mail.ru>
2022-07-29 17:33:56 +0300

    cd prototype

--- Cryptoserv opt/Server/test.py
+++ Cryptoserv opt/Server/test.py
@@ -12,11 +12,13 @@ def list(args=None):
 cur_path = os.path.dirname(__file__)
 print(cur_path)
 list(cur_path)
-new_path = os.path.join(os.path.dirname( __file__ ), 'templates').replace('\\', '/')
+new_path = os.path.join(os.path.dirname(__file__), 'test_folder').replace('\\', '/')
 if not os.path.isdir(new_path):
-    cur_path = new_path
+    new_path = cur_path
     print("Not a valid dir")
+else:
+    cur_path = new_path
 #  new_path = cur_path + "\\" + input()
-print(new_path)
-list(new_path)
+print(cur_path)
+list(cur_path)
 
